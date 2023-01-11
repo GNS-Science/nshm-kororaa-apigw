2674d4f8dd67d98c04c58ae114f21e97a655dcba
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-01-11 17:55:07 +1300

    finally got delegations going

--- nshm.demo.js
+++ nshm.demo.js
@@ -9,7 +9,7 @@ const {
   RenameTypes,
   RenameRootFields,
 } = require("@graphql-tools/wrap");
-const { stitchSchemas } = require("@graphql-tools/stitch");
+const { stitchSchemas, handleRelaySubschemas } = require("@graphql-tools/stitch");
 const { delegateToSchema } = require("@graphql-tools/delegate");
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
@@ -23,8 +23,17 @@ const KORORAA_TOKEN = process.env.NZSHM_KORORAA_GRAPHQL_API_KEY;
 const createRemoteSchema = async ({ url, headers, ...rest }) => {
   const executor = async ({ document, variables }) => {
     const query = typeof document === "string" ? document : print(document);
-    console.log('query', document, url)
-    return await rawRequest(url, query, variables, headers);
+    // console.log('query', document, url)
+    console.log('in executor')
+    console.log('query:')
+    console.log(query)
+    const resp = await rawRequest(url, query, variables, headers);
+    debugger;
+    console.log('response:')
+    console.log(resp)
+    console.log(resp.data)
+
+    return resp
   };
 
   const schema = wrapSchema({
@@ -39,13 +48,19 @@ const createRemoteSchema = async ({ url, headers, ...rest }) => {
 
 const localSchema = makeExecutableSchema({
   typeDefs: /* GraphQL */ `
+    type Model {
+      version: String
+    }
+
     type Query {
       hello: String
+      current_model: Model
     }
   `,
   resolvers: {
     Query: {
-      hello: () => 'It\'s a Brave New World in Stitch land'
+      hello: () => 'It\'s a Brave New World in Stitch land',
+      current_model: () =>  {return { version: 'NSHM_1.0.0' }}
     }
   },
 })
@@ -56,48 +71,81 @@ const stitched_schema = async () => {
     schema: localSchema,
     transforms: [
       new RenameRootFields((_, fieldName) => `NSHM_${fieldName}`),
+      new RenameTypes((name) => `NSHM_${name}`),
     ],
   }
 
   const kororaaSchema = await createRemoteSchema({
-    url: KORORAA_API,
-    headers: {'X-API-KEY': KORORAA_TOKEN},
-    transforms: [
-      new RenameRootFields((_, fieldName) => `KORORAA_${fieldName}`),
-      new RenameTypes((name) => `KORORAA_${name}`),
-    ],
-  });
+      url: KORORAA_API,
+      headers: {'X-API-KEY': KORORAA_TOKEN},
+      transforms: [
+        new RenameRootFields((_, fieldName) => `KORORAA_${fieldName}`),
+        new RenameTypes((name) => `KORORAA_${name}`),
+      ]
+    });
 
-  const toshiSchema = await createRemoteSchema({
+  const toshiSchema =  await createRemoteSchema({
     url: TOSHI_API,
     headers: {'X-API-KEY': TOSHI_TOKEN},
     transforms: [
       new RenameRootFields((_, fieldName) => `TOSHI_${fieldName}`),
       new RenameTypes((name) => `TOSHI_${name}`),
-    ],
-  });
+      ],
+    });
+
+  // console.log('kororaaSchema', kororaaSchema)
 
   const schema = await stitchSchemas({
+    // subschemas: handleRelaySubschemas(
+    //   [kororaaSchema, toshiSchema], //, localSubschema],
+    //   id => getTypeNameFromGlobalID(id)
+    // ),
     subschemas: [kororaaSchema, toshiSchema, localSubschema],
     typeDefs: `
       extend type KORORAA_SourceLogicTreeBranch {
         source_solution: TOSHI_SourceSolutionUnion
       }
+      extend type NSHM_Model{
+        model: KORORAA_NzshmModelResult
+      }
     `,
     resolvers: {
+
+      NSHM_Model: {
+        model:  {
+          selectionSet: '{ version }',
+          resolve: (parent, args, context, info) => {
+            console.log('resolve', parent, parent.version, args)
+            return delegateToSchema({
+              schema: kororaaSchema,
+              operation: "query",
+              fieldName: "KORORAA_nzshm_model",
+              args: {
+                version: parent.version,
+              },
+              context,
+              info,
+            });
+          },
+        }
+      },
+
       KORORAA_SourceLogicTreeBranch: {
         source_solution: {
-          selectionSet: `{ id }`,
+          selectionSet: `{ inversion_solution_id }`, //
           resolve: (parent, args, context, info) => {
             console.log('resolve', parent.inversion_solution_id, args)
+            // console.log( context )
+            // console.log( info )
+
+            if (parent.inversion_solution_id === null)  return null
+
             return delegateToSchema({
               schema: toshiSchema,
               operation: "query",
-              fieldName: "TOSHI_Node",
+              fieldName: "TOSHI_node",
               args: {
-                where: {
                   id: parent.inversion_solution_id,
-                },
               },
               context,
               info,
@@ -105,7 +153,7 @@ const stitched_schema = async () => {
           },
         },
       },
-    },
+    }, //resolvers
   });
 
   return schema
