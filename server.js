a2c766e202d7c721789319878b642f2af951d46e
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-01-13 16:33:36 +1300

    configure for  serverless;

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const serverless = require('serverless-http');
 const express = require("express");
 const { createYoga } = require('graphql-yoga');
 const { buildSubschemaConfigs, buildGatewaySchema } = require('./lib/schema_builder');
@@ -9,13 +10,15 @@ const stitched_schema = buildGatewaySchema(subschemaConfigs);
 
 const yoga = createYoga({
    schema: stitched_schema,
-   graphiql: true,
+   graphiql: { endpoint: '/dev/graphql' }
 })
 
 const app = express();
 
 app.use('/graphql', yoga);
 
-app.listen(process.env.PORT || 4002, () => {
-  console.log("Server started!");
-});
+// app.listen(process.env.PORT || 4002, () => {
+//   console.log("Server started!");
+// });
+
+module.exports.handler = serverless(app);
