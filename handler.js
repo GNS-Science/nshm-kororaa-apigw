9263de18ceb07e0f6ee7b163ac7b5749dbf6754d
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-01-18 10:34:00 +1300

    local working with serverless path and graphql;

--- handler.js
+++ handler.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { configure } = require('@vendia/serverless-express')
 const { createYoga } = require('graphql-yoga');
 const { buildSubschemaConfigs, buildGatewaySchema } = require('./lib/schema_builder');
@@ -7,11 +8,17 @@ const { config } = require('./gateway_config');
 const subschemaConfigs = buildSubschemaConfigs(config);
 const stitched_schema = buildGatewaySchema(subschemaConfigs);
 
+const rootPath = `/${process.env.BASE_PATH}/graphql` // must fit serverless.yml eg `/X/{any+}`
+
+// const graphqlEndpoint = (process.env.IS_OFFLINE === "true" ? `/${process.env.DEPLOYMENT_STAGE}${rootPath}`  :  rootPath)
+
+// console.log('graphqlEndpoint', graphqlEndpoint)
+
 const app = createYoga({
   graphiql: true,
-  graphqlEndpoint: `/kororaa-app-api/graphql`,
-  graphiql: { endpoint: `kororaa-app-api/graphql` },
-  // landingPage: false,
+  graphqlEndpoint: rootPath, // MUST MATCH path in serverlss.yml
+  graphiql: { endpoint: (process.env.IS_OFFLINE === "true" ? `/${process.env.DEPLOYMENT_STAGE}${rootPath}`  :  rootPath)},
+  landingPage: false,
   schema: stitched_schema,
 })
 
